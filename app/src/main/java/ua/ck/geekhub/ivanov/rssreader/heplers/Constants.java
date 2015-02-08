7d929f7e48c258df2ffac7ee27d900be5e8ae5a7
lborlandl <lborlandl2@gmail.com>
2015-02-08 18:57:10 +0200

    tried to fix problem

--- app/src/main/java/ua/ck/geekhub/ivanov/rssreader/heplers/Constants.java
+++ app/src/main/java/ua/ck/geekhub/ivanov/rssreader/heplers/Constants.java
@@ -1,7 +1,6 @@
 package ua.ck.geekhub.ivanov.rssreader.heplers;
 
 public class Constants {
-//    public static final String URL_JSON = "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&num=50&q=";
     public static final String URL_NEWS = "http://trashbox.ru/feed_topics/b_news/";
     public static final String EXTRA_STATE = "state";
     public static final String EXTRA_FEEDS_COUNT = "EXTRA_FEEDS_COUNT";
